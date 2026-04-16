e5df6973e1237b0211e39146aeee16984c9a3692
Arif Chowdhury Pulok <48258039+puloks@users.noreply.github.com>
2026-04-17 02:11:19 +0600

    Modify text color and emoji in the markdown section
    Updated the text color for the description paragraph and changed the emoji displayed.

--- main.py
+++ main.py
@@ -154,9 +154,9 @@ img {
 
 st.markdown("""
     <div style='text-align: center; margin-top: 1em;'>
-        <span style='font-size:2.5em;'>🌿🍂</span>
+        <span style='font-size:2.5em;'>🌱</span>
         <h1 style='color: #1565c0; margin-bottom:0;'>Leaf Disease Detection</h1>
-        <p style='color: #616161; font-size:1.15em;'>Upload a leaf image and instantly find out if your plant is healthy or affected, along with expert care tips.</p>
+        <p style='color: #000; font-size:1.15em;'>Upload a leaf image and instantly find out if your plant is healthy or affected, along with expert care tips.</p>
     </div>
 """, unsafe_allow_html=True)
 
